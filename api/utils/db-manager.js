b0dfa2b874add642ca1ee80d371e14d743684bc1
Ron <ronerlih@gmail.com>
2019-05-05 17:08:17 -0400

    mongo insert minor changes
    added comments and styling

--- api/utils/db-manager.js
+++ api/utils/db-manager.js
@@ -1,35 +1,35 @@
+// ## mongoDB atlas cluster manager
+// ## methods:
+// ##   insert => excepts a hash and message and inserts them as a key:value pair
+// ##   
+
 const MongoClient = require('mongodb').MongoClient;
 import {mongoUser, mongoPass} from '../../config';
-const test = require('assert');
-
-console.log('in db module');
-console.log('mongo user', mongoUser);
 
 const insert = ( hash, msg ) => {
+    // connect to mongo client (v4.0.9).
     const URL = `mongodb+srv://${mongoUser}:${mongoPass}@sandbox-l30bx.mongodb.net/?`;
     const client = new MongoClient(URL, { useNewUrlParser: true });
     client.connect( async (err, _client) => {
-
-        // if (err) throw (err);
-    try {
-        const collection = await _client.db("hash-service").collection("messages");
-        
-        const data = {};
-        data[hash] = msg;
-        // let documents = await collection.find( data ).toArray();
-        let documents = await collection.find( {[hash]: {$exists: true}} ).toArray();
-        console.log('documents.length', documents.length);
-        if (documents.length === 0){
-            await collection.insertOne( data );
-            _client.close(true);
-        } else {
-            console.log('document exists');
-            _client.close(true);
+        if (err) throw (err);
+        try {
+            const collection = await _client.db("hash-service").collection("messages");
+            const data = {[hash]: [msg]};
+            
+            // check if hash is already in db.
+            let documents = await collection.find( {[hash]: {$exists: true}} ).toArray();
+            // if so, add to db.
+            if (documents.length === 0){
+                await collection.insertOne( data );
+                _client.close(true);
+            } else {
+                console.log('document exists');
+                _client.close(true);
+            }
+        } catch (err){
+            errorPipe(err);
         }
-    } catch (err){
-        errorPipe(err);
-    }
-      });
+    });
     
 }
 
@@ -38,6 +38,7 @@ const errorPipe = ( err ) => {
     _client.close(true);
     throw err;
 }  
+
 module.exports = {
     insert: insert
 }
