3b7ec1738956c56469a1ded3fb676e6dfbf3ce9c
Ron <ronerlih@gmail.com>
2019-05-06 21:04:27 -0400

    git

--- __tests__/sorter.test.js
+++
@@ -1,9 +0,0 @@
-import sorter from '../api/sorter';
-
-let data = [1,2,2];
-
-describe('testing a test suit', () => {
-    it('Should log data', () => {
-        expect(typeof sorter( data )).toBe('object');
-    });
-});
